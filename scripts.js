f48cfe4371df7fab363a15e6c812bfad20777c48
Anastacio <gl0inn@hotmail.com>
2022-06-17 12:02:50 -0300

    updates

--- scripts.js
+++ scripts.js
@@ -25,76 +25,83 @@ function removeTransition(e){
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
 buttons.forEach(button => button.addEventListener('click', function play() {
-    if (this.id==='scissors'){
+    if (parseInt(playerscore.innerText.slice(-2)) === 5){
+        result.textContent='YOU WON'
+        this.removeEventListener('click', play)
+    }
+    else if (parseInt(cpuscore.innerText.slice(-2)) === 5){
+        result.textContent='CPU WON'
+        this.removeEventListener('click', play)
+    }
+    else if (this.id==='scissors'){
+        playRound(this.id)
         this.classList.add('playing')
         player.textContent = `You played: ${this.id}`;
         player.removeAttribute('class');
         player.classList.add(`${this.id}`);
-        let cpu = computerPlay();
-        if (cpu==='rock'){
-            result.textContent=('You lost! Rock beats Scissors!')
-            cpuscore.textContent = `CPU score: ${parseInt(cpuscore.innerText.slice(-2)) + 1}`
-        }
-        else if (cpu==='paper'){
-            result.textContent=('You won! Scissors beats Paper!')
-            playerscore.textContent = `Player score: ${parseInt(playerscore.innerText.slice(-2)) + 1}`
-        }
-        else{result.textContent=(`Thats a Tie!`)};
-        if (parseInt(playerscore.innerText.slice(-2)) === 5){
-            result.textContent='YOU WON'
-            this.removeEventListener('click', play)
-        }
-        else if (parseInt(cpuscore.innerText.slice(-2)) === 5){
-            result.textContent='CPU WON'
-            this.removeEventListener('click', play)
-        };
     }
     else if (this.id==='paper'){
         this.classList.add('playing')
         player.textContent = `You played: ${this.id}`;
         player.removeAttribute('class');
         player.classList.add(`${this.id}`);
-        let cpu = computerPlay();
-        if (cpu ==='rock'){
-            result.textContent=('You won! Paper beats Rock!')
-            playerscore.textContent = `Player score: ${parseInt(playerscore.innerText.slice(-2)) + 1}`
-        }
-        else if (cpu==='scissors') {
-            result.textContent=('You lost! Scissors beats Paper!')
-            cpuscore.textContent = `CPU score: ${parseInt(cpuscore.innerText.slice(-2)) + 1}`
-        }
-        else{result.textContent=(`Thats a Tie!`)};
-        if (parseInt(playerscore.innerText.slice(-2)) === 5){
-            result.textContent='YOU WON'
-            this.removeEventListener('click', play)
-        }
-        else if (parseInt(cpuscore.innerText.slice(-2)) === 5){
-            result.textContent='CPU WON'
-            this.removeEventListener('click', play)
-        };
+        playRound(this.id)    
     } 
     else if (this.id==='rock') {
         this.classList.add('playing')
         player.textContent = `You played: ${this.id}`;
         player.removeAttribute('class');
         player.classList.add(`${this.id}`);
-        let cpu = computerPlay();
-        if (cpu==='paper'){
-            result.textContent=('You lost! Paper beats Rock!')
-            cpuscore.textContent = `CPU score: ${parseInt(cpuscore.innerText.slice(-2)) + 1}`
-        }
-        else if (cpu==='scissors'){
-            result.textContent=('You won! Rock beats Scissors')
-            playerscore.textContent = `Player score: ${parseInt(playerscore.innerText.slice(-2)) + 1}`
-        }
-        else{result.textContent=(`Thats a Tie!`)};
-        if (parseInt(playerscore.innerText.slice(-2)) === 5){
-            result.textContent='YOU WON'
-            this.removeEventListener('click', play)
-        }
-        else if (parseInt(cpuscore.innerText.slice(-2)) === 5){
-            result.textContent='CPU WON'
-            this.removeEventListener('click', play)
-        };
+        playRound(this.id)
     };
 }));
+
+
+let cpucount = 0
+let playercount = 0
+function playRound(click){
+    if (click==='scissors'){    
+        let cpu = computerPlay();
+        if (cpu==='rock'){
+            cpucount++
+            result.textContent=('You lost! Rock beats Scissors!')
+            cpuscore.textContent = `CPU score: ${cpucount}`
+        }
+        else if (cpu==='paper'){
+            playercount++
+            result.textContent=('You won! Scissors beats Paper!')
+            playerscore.textContent = `Player score: ${playercount}`
+        }
+        else{result.textContent=(`Thats a Tie!`)};
+    }
+    else if(click==='rock'){
+        let cpu = computerPlay();
+        if (cpu==='paper'){
+            cpucount++
+            result.textContent=('You lost! Paper beats Rock!')
+            cpuscore.textContent = `CPU score: ${cpucount}`
+        }
+        else if (cpu==='scissors'){
+            playercount++
+            result.textContent=('You won! Rock beats Scissors')
+            playerscore.textContent = `Player score: ${playercount}`
+        }
+        else{result.textContent=(`Thats a Tie!`)};
+    }
+    else if(click==='paper'){
+        let cpu = computerPlay();
+        if (cpu ==='rock'){
+            playercount++
+            result.textContent=('You won! Paper beats Rock!')
+            playerscore.textContent = `Player score: ${playercount}`
+        }
+        else if (cpu==='scissors') {
+            cpucount++
+            result.textContent=('You lost! Scissors beats Paper!')
+            cpuscore.textContent = `CPU score: ${cpucount}`
+        }
+        else{result.textContent=(`Thats a Tie!`)};   
+    };
+};
+console.log(playercount)
+
